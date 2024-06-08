b82df0655457242ba832a8c56caa47e7dee383cf
bubbarob19 <77255872+bubbarob19@users.noreply.github.com>
2024-06-08 14:07:16 -0500

    Initial commit

---
+++ src/main/java/me/bubbarob19/pongrating/PongratingApplication.java
@@ -0,0 +1,13 @@
+package me.bubbarob19.pongrating;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PongratingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PongratingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/bubbarob19/pongrating/PongratingApplicationTests.java
@@ -0,0 +1,13 @@
+package me.bubbarob19.pongrating;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PongratingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
